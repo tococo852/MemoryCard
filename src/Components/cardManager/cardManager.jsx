58dca763b1a4e96acf3d3f5a6512b4bec621e210
tococo852 <tococo852@gmail.com>
2025-11-19 18:16:43 -0300

    set up api fetch module

--- src/Components/cardManager/cardManager.jsx
+++ src/Components/cardManager/cardManager.jsx
@@ -47,7 +47,8 @@ const CardManager = ({
 
     useEffect(()=>{
         const load= async()=>{
-            const pictureSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            //const pictureSet = await PictureImport()
+            const pictureSet= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
             setPictures(shuffle(pictureSet))
             setIsLoading(false)
         }
--- src/Components/pictureImport/pictureImport.jsx
+++ src/Components/pictureImport/pictureImport.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react"
 
- const PictureImport=()=>{
+ const PictureImport = async()=>{
 
+    let pokelist=['lopunny','gardevoir','delphox']
+    let spritelist=[]
+
+    let pokemon='ditto'
+    let pokeData= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    let spriteData= await pokeData.json()
+    let spriteImg= spriteData.sprites.front_default
+
+    return spriteImg
 
 }
+
+console.log(  PictureImport())
 
 export{PictureImport}
